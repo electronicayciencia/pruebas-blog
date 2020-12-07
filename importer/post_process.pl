c84ca584ad5cd7253b7447780f297b0b24982d7a
Electronicayciencia <electronicayciencia@gmail.com>
2020-12-07 15:17:31 +0100

    Improved formatting.

--- importer/post_process.pl
+++ importer/post_process.pl
@@ -89,7 +89,7 @@ sub format_pre {
 
 	if ($block =~ m{name="code"}) {
 	    ($lang) = $block =~ /class="([^"]+)"/;
-	  	print STDERR "Code block: $lang\n";
+		#print STDERR "Code block: $lang\n";
 	}
 
 	if (not $block =~ m{<pre[^>]*>(.*?)</pre>}ms) {
@@ -99,9 +99,7 @@ sub format_pre {
 
 	my $c = $1;
 
-	$c =~ s{<br[^>]+>([^\n])}{\n$1}mg;
-	$c =~ s{<br[^>]+>$}{}mg;
-	#$c =~ s{^}{    }mg;
+	$c =~ s{<br[^>]+>}{\n}mg;
 
 	$c =~ s{&lt;}{<}g;
 	$c =~ s{&gt;}{>}g;
@@ -113,6 +111,23 @@ sub format_pre {
 	$c =~ s{<em>}{}g;
 	$c =~ s{</em>}{}g;
 
+	# prevent empty line at the end of the block
+	$c =~ s{\s+$}{}g;
+	$c =~ s{^\s+}{}g;
+	
+	# Blogspot syntax for perl does not exist. Is cpp marked but is it actually perl?
+	if ($c =~ m{my\s+\$}) {
+	    $lang = "perl";
+	}
+
+	if ($c =~ m{subplot} or $c =~ m{^\s*%} or $c =~ m{\w+\(:,\d+\)}) {
+	    $lang = "matlab";
+	}
+
+	if ($c =~ m{#define}) {
+	    $lang = "c";
+	}
+	
 	return "\n\n```$lang\n$c\n```\n\n";
 }
 
@@ -152,11 +167,66 @@ sub format_ol {
 	return "\n".join("\n", @items)."\n";
 }
 
+# Pre lines in markdown cannot have tags
+sub format_preline {
+	my $line = shift;
+
+	$line =~ s{<span[^>]*>}{}g;
+	$line =~ s{</span>}{}g;
+	$line =~ s{<b>}{}g;
+	$line =~ s{</b>}{}g;
+	$line =~ s{<em>}{}g;
+	$line =~ s{</em>}{}g;
+	$line =~ s{<br[^>]*>}{\n}g;
+
+	return $line;
+}
+
+sub format_spanmonospace {
+	my $block = shift;
+
+	# Remove format tags
+	$block =~ s{<span[^>]*>}{}g;
+	$block =~ s{</span>}{}g;
+	$block =~ s{<b>}{}g;
+	$block =~ s{</b>}{}g;
+	$block =~ s{<em>}{}g;
+	$block =~ s{</em>}{}g;
+	$block =~ s{&lt;}{<}g;
+	$block =~ s{&gt;}{>}g;
+	$block =~ s{&amp;}{&}g;
+
+	# It's only a line or multiple lines?
+	if ($block =~ /\n/ or $block =~ /<br[^>]*>/) {
+		$block =~ s{<br[^>]*>}{\n}g;
+		return "\n\n```\n$block\n```\n\n";
+	}
+	else {
+		return "    $block";
+	}
+}
+
+sub format_blockquote {
+	my $block = shift;
+
+	# Remove format tags
+	$block =~ s{<span[^>]*>}{}g;
+	$block =~ s{</span>}{}g;
+
+	$block =~ s{<br[^>]*>}{\n}g;
+	$block =~ s{^}{> }gm;
+	
+	return "\n\n$block\n\n";
+}
 
 
 sub process_body {
 	my $s = shift;
 	$s = trim($s);
+
+	# Particular case correction:
+	$s =~ s{Exploit K</b>it}{Exploit Kit</b>}g;
+
 
 	# Images with caption and link
 	# <table ... <a href="...blogspot.com/.../div_by_5.png" src="...blogspot.com/.../div_by_5.png" width="640" ...>CAPTION</td></tr></tbody></table>
@@ -169,7 +239,6 @@ sub process_body {
 	# Direct images, no caption, no link
 	# <img ... src="http://2.bp.blogspot.com/.../db9_null_loop.png" ... />
 	$s =~ s{<img.*?src=".*?bp.blogspot.com.*?/([^/"]+)".*?/>}{image_string($1,"","")}sge;
-	
 
 	# Remove feed line
 	# <div class="blogger-post-footer">...</div>
@@ -181,25 +250,33 @@ sub process_body {
 	# Remove ancor more: <a name='more'></a>
 	$s =~ s{<a name='more'></a>}{}g;
 
+	# Remove color styles in spans
+	$s =~ s{style="color: [^"]+"}{}g;
+
 	# Remove empty divs
 	$s =~ s{<div[^>]*>\s*</div>}{}mg;
+	$s =~ s{<div[^>]*><br[^>]+></div>}{\n}mg;
 	
 	# remove empty spans
 	$s =~ s{<span[^>]*>\s*</span>}{}mg;
+	$s =~ s{<span[^>]*><br[^>]+></span>}{\n}mg;
+
+	# some <br> cases
+	$s =~ s{</span><br[^>]*>}{</span>\n}mg;
 
 	# Remove extrange garbage
-	$s =~ s{<b><br /></b>}{<br />}g;
-	
+	$s =~ s{<b><br[^>]*></b>}{<br />}g;
+	$s =~ s{<em><br[^>]*></em>}{<br />}g;
+
+	# Remove stiles from simple tags
+	$s =~ s{<b [^>]*>}{<b>}g;
+	$s =~ s{<i [^>]*>}{<i>}g;
+
+	# Replace uncommon format tags
 	$s =~ s{<k>}{<em>}g;
 	$s =~ s{</k>}{</em>}g;
 	$s =~ s{<i>}{<em>}g;
 	$s =~ s{</i>}{</em>}g;
-
-	# Trailing or leading spaces in tags
-	$s =~ s{<b> }{ <b>}g;
-	$s =~ s{ </b>}{</b> }g;
-	$s =~ s{<em> }{ <em>}g;
-	$s =~ s{ </em>}{</em> }g;
 
 	# Remove divs, any divs
 	$s =~ s{<div[^>]*>}{}g;
@@ -207,6 +284,11 @@ sub process_body {
 
 	# Remove nbsp?
 	$s =~ s{&nbsp;}{ }g;
+	
+	# Remove empty formats
+	$s =~ s{<b>\s*</b>}{}g;
+	$s =~ s{<em>\s*</em>}{}g;
+
 
 	# These internal links are incorrect
 	$s =~ s{2010/06/difraccion-en-un-dvd}{2010/07/difraccion-en-un-dvd}g;
@@ -216,40 +298,22 @@ sub process_body {
 	$s =~ s{<a href="[^"]+electronicayciencia.blogspot.com/(.+?).html">(.*?)</a>}{link_post_to_local($1, $2)}gmse;
 	$s =~ s{<a href="[^"]+electronicayciencia.blogspot.com.es/(.+?).html">(.*?)</a>}{link_post_to_local($1, $2)}gmse;
 	
-	# Unordered list format
-	#$s =~ s{<ul>}{\n<ul>\n}g;
-	#$s =~ s{(<li>.*?</li>)}{  $1\n}g;
-	#$s =~ s{</ul>}{</ul>\n\n}g;
-	
 	# Format unordered list blocks
 	$s =~ s{(<ul>.*?</ul>)}{format_ul($1)}msge;
-
-	
-	# Ordered list format
-	#$s =~ s{<ol>}{\n<ol>\n}g;
-	#$s =~ s{(<li>.*?</li>)}{  $1\n}g;
-	#$s =~ s{</ol>}{</ol>\n\n}g;
 	
 	# Format unordered list blocks
 	$s =~ s{(<ol>.*?</ol>)}{format_ol($1)}msge;
-
-	# <blockquote></blockquote>
-	$s =~ s{<blockquote>}{\n<blockquote>\n}g;
-	$s =~ s{</blockquote>}{\n</blockquote>\n}g;
-	
-	# Pre block in its own line
-	#$s =~ s{(<pre[^>]*>)}{\n$1}g;
-	#$s =~ s{</pre>}{</pre>\n}g;
 
 	# Format pre blocks
 	$s =~ s{(<pre.*?</pre>)}{format_pre($1)}msge;
 
 	# convert span monospaced into pre blocks
-	$s =~ s{<span[^>]*monospace[^>]*>(.*?)</span>}{    $1\n}g;
+	$s =~ s{<span[^>]*monospace[^>]*>(.*?)</span>}{format_spanmonospace($1)}mge;
+	$s =~ s{<br />    }{\n\n    }g;
 	
 	# keep a blank line between indented blocks and next line
 	$s =~ s{(^\S\S\S\S.*)\n(    .*)}{$1\n\n$2}mg;
-	$s =~ s{(    .*)\n((?!    |\n).*)}{$1\n\n$2}mg;
+	$s =~ s{^(    .*)\n((?!    |\n).*)}{$1\n\n$2}mg;
 
 	# Equations
 	$s =~ s{\\\[<br[^>]*>}{\n\$\$\n}msg;
@@ -258,6 +322,14 @@ sub process_body {
 	$s =~ s{\\\[}{\n\$\$\n}msg;
 	$s =~ s{\\\]}{\n\$\$\n}msg;
 
+	# Remove extrange garbage, iteration II
+	$s =~ s{<b><br[^>]*></b>}{<br />}g;
+	$s =~ s{<em><br[^>]*></em>}{<br />}g;
+	$s =~ s{<span[^>]*><br[^>]*></span>}{<br />}g;
+
+	# <blockquote></blockquote>
+	$s =~ s{<blockquote[^>]*>(.+?)</blockquote>}{format_blockquote($1)}msge;
+	
 	# New paragraph, iteration II
 	$s =~ s{(<br[^>]*>\s*<br[^>]*>)}{\n\n}gms;
 	$s =~ s{\n<br[^>]*>}{\n\n}gms;
@@ -270,17 +342,31 @@ sub process_body {
 	$s =~ s{&amp;}{&}gms;
 
 	# Section titles
-	$s =~ s{\n<b>(.{1,80})</b>\n}{\n## $1\n}msg;
+	$s =~ s{\n<b>(\w.{1,80})</b>\n}{\n## $1\n}msg;
 
 	# Kill brs <- NO: needed in image captions
 	#$s =~ s{(<br[^>]*>)}{\n}gms;
+	
+	# Trailing or leading spaces in tags
+	$s =~ s{<b>(\s+)}{$1<b>}g;
+	$s =~ s{(\s+)</b>}{</b>$1}g;
+	$s =~ s{<em>(\s+)}{$1<em>}g;
+	$s =~ s{(\s+)</em>}{</em>$1}g;
+	$s =~ s{<span([^>]*)>(\s+)}{$2<span$1>}g;
+
+	# Remove format from pre lines (maybe redundant after block format function)
+	$s =~ s{^(    .*)}{format_preline($1)}ge;
+	
+	# Clear residual spans
+	$s =~ s{<span([^>]*)>}{}g;
+	$s =~ s{</span>}{}g;
+	
 
 	# Back-translate to Markdown
 	$s =~ s{<b>}{**}g;
 	$s =~ s{</b>}{**}g;
 	$s =~ s{<em>}{*}g;
 	$s =~ s{</em>}{*}g;
-
 	
 
 	return $s;
